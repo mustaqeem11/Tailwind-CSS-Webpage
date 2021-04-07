fbfb6ad58d3e94f7ac7c8adb294dd6c5e023f34e
mustaqeem11 <mustaqeem11@hotmail.co.uk>
2021-04-07 19:42:27 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      body: ['Inter', 'sans-serif']
+    },
+    extend: {
+      margin: {
+        7: '1.75rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
